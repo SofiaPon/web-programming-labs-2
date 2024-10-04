ad35d97346adf96e0c14abe51e3d867b0f3fe13e
Ponomareva Sofia <smailik19980@mail.ru>
2024-10-04 13:09:48 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -190,7 +190,10 @@ def example():
     return render_template('example.html', name=name, lab_number2=lab_number2, group=group, course=course, fruits=fruits)
     
 
-
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
+    
 
 @app.route('/lab2/books')
 def books():   
