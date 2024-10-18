40a4fb07dffd4e8686cf6bb41c830054bec8e759
Ponomareva Sofia <smailik19980@mail.ru>
2024-10-18 18:56:56 +0700

    Рефакторинг: выделение второй лабораторной

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect
 from lab1 import lab1
+from lab2 import lab2
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+@app.route("/")
+def start():
+    return redirect('/lab2/', code=302)
+
+
 app.register_blueprint(lab1)
+app.register_blueprint(lab2)
 
+if __name__ == "__main__":
+    app.run(debug=True)
