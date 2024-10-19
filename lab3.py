8985402bc39d768102005c738c56da3d45da262a
Ponomareva Sofia <smailik19980@mail.ru>
2024-10-19 21:53:24 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -53,3 +53,46 @@ def del_cookie():
     resp.delete_cookie('age') 
     resp.delete_cookie('name_color')
     return resp
+
+@lab3.route('/lab3/form1')
+def form1():
+    errors={}
+    user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+    erorrs={}
+    age = request.args.get('age')
+    if user == '':
+        errors['age'] = 'Заполните поле!'
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    # Пусть кофе стоит 120 рублей, черный чай - 80, зеленый - 70 рублей
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    # Добавка молока удоражает напиток на 30 рублей, а сахара - на 10
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('lab3/pay.html', price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    # Получаем сумму из запроса (она была рассчитана на странице оплаты)
+    price = request.args.get('price')
+    return render_template('lab3/success.html', price=price)
