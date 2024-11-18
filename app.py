506a73d6ed8a71881846fcba1bd0a4151f54ac94
Ponomareva Sofia <smailik19980@mail.ru>
2024-11-18 09:38:52 +0700

    Правки для SQLite

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']=os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
+app.config['DB_TYPE']=os.getenv('DB_TYPE', 'postgres')
 
 app.secret_key= 'секретно-секретный секрет'
 @app.route("/")
@@ -17,8 +21,9 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
-@app. route("/menu")
+@app.route("/menu")
 def menu():
     return """
 <!doctype html>
@@ -38,6 +43,7 @@ def menu():
             <li><a href='http://127.0.0.1:5000/lab3'>Третья лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab3'>Третья лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab4/'>Четвертая лабораторная</a></li>
+            <li><a href='http://127.0.0.1:5000/lab5/'>Пятая лабораторная</a></li>
         <ol>
         </main>
         <footer>
