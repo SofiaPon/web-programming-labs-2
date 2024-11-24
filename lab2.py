bfc59d0bff9ec67082accb6a70c8d0eabc84e67e
Ponomareva Sofia <smailik19980@mail.ru>
2024-11-24 19:28:22 +0700

    Заготовка к 6-ой лабораторной

--- lab2.py
+++ lab2.py
@@ -24,6 +24,8 @@ def menu():
             <li><a href='/lab2'>Вторая лабораторная</a></li>
             <li><a href='/lab3'>Третья лабораторная</a></li>
             <li><a href='/lab4'>Четвертая лабораторная</a></li>
+            <li><a href='/lab5'>Пятая лабораторная</a></li>
+            <li><a href='/lab6'>Шестая лабораторная</a></li>
         </ol>
         </main>
         <footer>
