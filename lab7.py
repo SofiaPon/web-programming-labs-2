e5eb66b77fb8cd1db2003b46d2c4ee259d132213
Ponomareva Sofia <smailik19980@mail.ru>
2024-12-22 23:40:59 +0700

    Вывод таблицы

--- lab7.py
+++ lab7.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, session, abort
 
 lab7 = Blueprint('lab7', __name__)
 
-@lab7.route('/lab7')
+@lab7.route('/lab7/')
 def main():
     return render_template('lab7/lab7.html')
 
