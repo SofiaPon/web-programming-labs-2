3f3e18bfdfc0ae5dec5d56d456a8a652893b93b3
Ponomareva Sofia <smailik19980@mail.ru>
2024-12-23 09:48:35 +0700

    Самостоятельное задание

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -8,16 +8,16 @@ function fillFilmList() {
         tbody.innerHTML = '';
         for(let i = 0; i < films.length; i++) {
             let tr = document.createElement('tr');
-
-            let tdTitle = document.createElement('td'); 
             let tdTitleRus = document.createElement('td'); 
+            let tdTitle = document.createElement('td'); 
             let tdYear = document.createElement('td');
             let tdActions = document.createElement('td');
 
 
-            tdTitle.innerText = films[i].title == films[i].title_ru ? '' : films[i].title;
             tdTitleRus.innerText = films[i].title_ru; 
+            tdTitle.innerHTML = films[i].title ? `<i>(${films[i].title})</i>` : '';  // показываем оригинальное название курсивом с скобками
             tdYear.innerText = films[i].year;
+
 
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
@@ -39,8 +39,9 @@ function fillFilmList() {
             tdActions.append(editButton); 
             tdActions.append(delButton);
 
-            tr.append(tdTitle);
+            
             tr.append(tdTitleRus);
+            tr.append(tdTitle);
             tr.append(tdYear);
             tr.append(tdActions);
 
