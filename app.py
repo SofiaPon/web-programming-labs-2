19b5b98d48a2b29e39ff4d46ba6be3bc33e98e79
Ponomareva Sofia <smailik19980@mail.ru>
2024-12-23 16:58:10 +0700

    Заготовка к 8-ой лабораторной

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 
 app = Flask(__name__)
@@ -28,6 +29,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 @app.route("/menu")
 def menu():
@@ -52,6 +54,7 @@ def menu():
             <li><a href='http://127.0.0.1:5000/lab5/'>Пятая лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab6/'>Шестая лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab7/'>Седьмая лабораторная</a></li>
+            li><a href='http://127.0.0.1:5000/lab8/'>Восьмая лабораторная</a></li>
         <ol>
         </main>
         <footer>
