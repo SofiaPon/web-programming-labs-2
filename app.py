6c7c0817b477416b010043efd4782f595c80a712
София пономарева <smailik19980@mail.ru>
2024-12-25 01:25:24 +0700

    ргз

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from rgz import rgz
 
 
 app = Flask(__name__)
@@ -16,11 +17,6 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-сек
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres') 
 
 app.secret_key= 'секретно-секретный секрет'
-@app.route("/")
-@app.route("/index")
-def start():
-    return redirect ('/menu', code=302)
-
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -30,7 +26,12 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(rgz)
 
+@app.route("/")
+@app.route("/index")
+def start():
+    return redirect ('/menu', code=302)
 @app.route("/menu")
 def menu():
     return """
@@ -43,18 +44,18 @@ def menu():
         <header>
             НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
         </header>
-        
+
         <main>
         <ol>
             <li><a href="http://127.0.0.1:5000/lab1">Первая лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab2'>Вторая лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab3'>Третья лабораторная</a></li>
-            <li><a href='http://127.0.0.1:5000/lab3'>Третья лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab4/'>Четвертая лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab5/'>Пятая лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab6/'>Шестая лабораторная</a></li>
             <li><a href='http://127.0.0.1:5000/lab7/'>Седьмая лабораторная</a></li>
-            li><a href='http://127.0.0.1:5000/lab8/'>Восьмая лабораторная</a></li>
+            <li><a href='http://127.0.0.1:5000/lab8/'>Восьмая лабораторная</a></li>
+            <li><a href='http://127.0.0.1:5000/rgz/'>РГЗ</a></li>
         <ol>
         </main>
         <footer>
@@ -63,6 +64,3 @@ def menu():
     </body>
 </html>
 """
-
-if __name__ == "__main__":
-    app.run(debug=True)
