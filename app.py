be30ed3509453ab60f04051e617826b3299c7208
Ponomareva Sofia <smailik19980@mail.ru>
2024-12-28 01:49:36 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
-from flask import Flask, url_for
+from flask import Flask, redirect, url_for
 import os
 from os import path
-from flask_sqlalchemy import SQLAlchemy
 from db import db
-from db.models import users, articles
-from flask import Flask, redirect, url_for
-from lab1 import lab1
-from lab2 import lab2
-from lab3 import lab3
-from lab4 import lab4
-from lab5 import lab5
-from lab6 import lab6
-from lab7 import lab7
-from lab8 import lab8
-from rgz import rgz
+from flask_sqlalchemy import SQLAlchemy
+from db.models import users
 
 
 app = Flask(__name__)
@@ -27,19 +17,26 @@ if app.config['DB_TYPE'] == 'postgres':
     db_password = '123'
     host_ip = '127.0.0.1'
     host_port = 5432
-    
+
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+
 else:
     dir_path = path.dirname(path.realpath(__file__))
-    db_path = path.join(dir_path, 'sofia_ponomareva.db')
+    db_path = path.join(dir_path, 'sofia_ponomareva_orm.db')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
+db.init_app(app)
 
+from lab1 import lab1
+from lab2 import lab2
+from lab3 import lab3
+from lab4 import lab4
+from lab5 import lab5
+from lab6 import lab6
+from lab7 import lab7
+from lab8 import lab8
+from rgz import rgz
 
-db.init_app(app) 
-
-
-app.secret_key= 'секретно-секретный секрет'
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
