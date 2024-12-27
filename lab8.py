12e607d2d5c1b2037379ca8e6828e9a45973f6fb
Ponomareva Sofia <smailik19980@mail.ru>
2024-12-28 02:25:32 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, session
+from db.models import users, articles
+from flask_login import login_user, login_required, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from db import db
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -7,9 +11,29 @@ def lab8_main():
     user = "anonymous"  # Пока пользователь анонимный
     return render_template('lab8/lab8.html', user=user)
 
-@lab8.route('/login')
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
 def login():
-    return "Страница входа (в разработке)"
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+    
+    user = users.query.filter_by(login = login_form).first()
+    if not login_form or not password_form:
+        return render_template('lab8/login.html', error='Заполните все поля')
+    
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember= False)
+            return redirect('/lab8')
+    return render_template('/lab8/login.html', error = 'Ошибка входа: логин и/или пароль неверны')
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Список статей"
+
 
 @lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
@@ -20,7 +44,7 @@ def register():
         password_form = request.form.get('password')
     # Проверки на пустые значения
         if not login_form or not password_form:
-            return render_template('lab8/register.html', error='Логин и пароль не должны быть пустыми')
+            return render_template('lab8/register.html', error='Поля логина и пароля обязательны')
     # Проверка существования пользователя с таким логином
         login_exists = users.query.filter_by(login=login_form).first()
         if login_exists:
@@ -32,9 +56,6 @@ def register():
         db.session.commit()
         return redirect('/lab8/')
 
-@lab8.route('/articles')
-def articles():
-    return "Список статей (в разработке)"
 
 @lab8.route('/create')
 def create_article():
