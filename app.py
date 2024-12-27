c7360bd93ee49e34ec5fe682e3743689cef30f26
Ponomareva Sofia <smailik19980@mail.ru>
2024-12-28 03:01:48 +0700

    Самостоятельное задание

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ login_manager.init_app(app)
 def load_users(login_id):
     return users.query.get(int(login_id))
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'very-secure-secret-key-here') 
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres') 
 
 if app.config['DB_TYPE'] == 'postgres':
@@ -26,6 +26,7 @@ if app.config['DB_TYPE'] == 'postgres':
     db_password = '123'
     host_ip = '127.0.0.1'
     host_port = 5432
+    options='-c client_encoding=UTF8'
 
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
 
